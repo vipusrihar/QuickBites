5bd2d55a8fa2263b6827a36e3bb1900470589a12
vipusrihar <vipusrihar@gmail.com>
2025-04-22 21:21:01 +0530

    file path changed

--- src/main/java/com/vipusa/onlineFood/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.vipusa.onlineFood.config;
-
-public class JwtConstant {
-    public static final String SECRET_KEY = "a-very-long-secret-key-that-is-at-least-64-bytes-long-1234567890";
-    public static final String JWT_HEADER = "Authorization";
-}
--- src/main/java/com/vipusa/onlineFood/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vipusa.onlineFood.repository;
-
-import com.vipusa.onlineFood.model.FoodOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<FoodOrder,Long> {
-}
